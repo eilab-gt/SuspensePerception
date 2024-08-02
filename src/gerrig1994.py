bb3b76bcbd05520dfed3cf32da996c7bcfd86d12
Glenn Matlin <10365809+glennmatlin@users.noreply.github.com>
2024-08-02 19:55:48 -0400

    chores: gerrig1994.md with paper text, the .py file with the prompts text etc

---
+++ src/gerrig1994.py
@@ -0,0 +1,191 @@
+experiment_A_prompt = """The following is an excerpt from Fleming’s first James Bond novel, "Casino Royale". In this book, Bond has been assigned to ‘ruin’ a criminal figure named Le Chiffre by, as it happens, causing Le Chiffre to lose a considerable amount of money gambling. Along the way, Bond has acquired a lady interest named Vesper. Although Bond has, in fact, brought about the gambling losses, Le Chiffre has laid a successful trap for Bond. Bond and Vesper are now the prisoners of Le Chiffre and his two gunmen.
+
+\"\"\"
+{STORY}
+\"\"\"
+
+Use the passage above to answer the following questions:
+
+***Question 1: "How likely is it that Bond will escape from Le Chiffre?"***
+1. Not very likely
+2. Somewhat likely
+3. Moderately likely
+4. Likely
+5. Quite likely
+6. Very likely
+7. Extremely likely
+
+***Question 2: "How suspenseful do you find this passage to be?"***
+1. Not very suspenseful
+2. Somewhat suspenseful
+3. Moderately suspenseful
+4. Suspenseful
+5. Quite suspenseful
+6. Very suspenseful
+7. Extremely suspenseful
+
+Answer the questions. At end of your response you must re-state your answer choices in the format:
+```
+Q1: [1-7]
+Q2: [1-7]
+```
+"""
+
+experiment_B_prompt = """The following is an excerpt from Fleming’s first James Bond novel, "Casino Royale". In this book, Bond has been assigned to ‘ruin’ a criminal figure named Le Chiffre by, as it happens, causing Le Chiffre to lose a considerable amount of money gambling. Along the way, Bond has acquired a lady interest named Vesper. Although Bond has, in fact, brought about the gambling losses, Le Chiffre has laid a successful trap for Bond. Bond and Vesper are now the prisoners of Le Chiffre and his two gunmen.
+\"\"\"
+{STORY}
+\"\"\"
+
+Use the passage above to answer the following questions:
+
+***Question 1: "How likely is it that Bond will escape from Le Chiffre?"***
+1. Not very likely
+2. Somewhat likely
+3. Moderately likely
+4. Likely
+5. Quite likely
+6. Very likely
+7. Extremely likely
+
+***Question 2: "How suspenseful do you find this passage to be?"***
+1. Not very suspenseful
+2. Somewhat suspenseful
+3. Moderately suspenseful
+4. Suspenseful
+5. Quite suspenseful
+6. Very suspenseful
+7. Extremely suspenseful
+
+Answer the questions. At end of your response you must re-state your answer choices in the format:
+```
+Q1: [1-7]
+Q2: [1-7]
+```
+"""
+
+experiment_C_prompt = """The following is an excerpt from Ian Fleming’s tenth James Bond novel, "On Her Majesty's Secret Service". In this novel, Bond discovers the villain Blofeld is secretly conditioning young women to spread a virus deadly to agriculture and livestock across England. In this excerpt, Bond has been caught searching Blofeld's private office.
+\"\"\"
+{STORY}
+\"\"\"
+
+Use the passage above to answer the following questions:
+
+***Question 1: "How likely is it that Bond will escape from Blofeld?"***
+1. Not very likely
+2. Somewhat likely
+3. Moderately likely
+4. Likely
+5. Quite likely
+6. Very likely
+7. Extremely likely
+
+***Question 2: "How suspenseful do you find this passage to be?"***
+1. Not very suspenseful
+2. Somewhat suspenseful
+3. Moderately suspenseful
+4. Suspenseful
+5. Quite suspenseful
+6. Very suspenseful
+7. Extremely suspenseful
+
+Answer the questions. At end of your response you must re-state your answer choices in the format:
+```
+Q1: [1-7]
+Q2: [1-7]
+```
+"""
+
+experiment_A_pen_not_mentioned = """Le Chiffre was standing in the doorway of a room on the right. He crooked a finger at Bond in a silent, spidery summons.
+  Vesper was being led down a passage towards the back of the house. Bond suddenly decided. With a backward kick which connected with the thin man’s shins and brought a whistle of pain from him, Bond hurled himself down the passage after her. His plan was to do as much damage as possible to the two gunmen and be able to exchange a few hurried words with the girl.
+  Like lightning the Corsican slammed himself back against the wall of the passage and, as Bond’s foot whistled past his hip, he very quickly, but somehow delicately, shot out his left hand, caught Bond’s shoe at the top of its arc, and twisted it sharply. As he crashed to the ground, Bond rolled agilely and, with a motion in which he took great pride, he righted himself with minimal damage.
+  “Search him.” barked Le Chiffre.
+  The two gunmen dragged Bond to his feet. While the thin man kept his gun trained on Bond’s unquiet chest, the Corsican roughly stripped Bond’s revolver out of its shoulder holster. He twisted Bond around brusquely in search of other weaponry. Le Chiffre observed his assistant’s work attentively. Then, as if reading Bond’s thoughts, he crossed the room and said, “Come my dear friend. Let’s not waste time.”"""
+
+experiment_A_pen_mentioned_removed = """Le Chiffre was standing in the doorway of a room on the right. He crooked a finger at Bond in a silent, spidery summons.
+  Vesper was being led down a passage towards the back of the house. Bond suddenly decided. With a backward kick which connected with the thin man’s shins and brought a whistle of pain from him, Bond hurled himself down the passage after her. His plan was to do as much damage as possible to the two gunmen and be able to exchange a few hurried words with the girl.
+  Like lightning the Corsican slammed himself back against the wall of the passage and, as Bond’s foot whistled past his hip, he very quickly, but somehow delicately, shot out his left hand, caught Bond’s shoe at the top of its arc, and twisted it sharply. As he crashed to the ground, Bond rolled agilely and, with a motion that he hoped went unnoticed, moved his fountain pen deeper into his breast pocket.
+  “Search him.” barked Le Chiffre.
+  The two gunmen dragged Bond to his feet. While the thin man kept his gun trained on Bond’s unquiet chest, the Corsican roughly stripped Bond’s revolver out of its shoulder holster. He twisted Bond around brusquely in search of other weaponry. Le Chiffre observed his assistant’s work attentively. Then, as if reading Bond’s thoughts, he crossed the room and snatched away Bond’s fountain pen. “Come my dear friend,” said Le Chiffre. “Let’s not waste time.”"""
+
+experiment_A_pen_mentioned_not_removed = """  Le Chiffre was standing in the doorway of a room on the right. He crooked a finger at Bond in a silent, spidery summons.
+  Vesper was being led down a passage towards the back of the house. Bond suddenly decided. With a backward kick which connected with the thin man’s shins and brought a whistle of pain from him, Bond hurled himself down the passage after her. His plan was to do as much damage as possible to the two gunmen and be able to exchange a few hurried words with the girl.
+  Like lightning the Corsican slammed himself back against the wall of the passage and, as Bond’s foot whistled past his hip, he very quickly, but somehow delicately, shot out his left hand, caught Bond’s shoe at the top of its arc, and twisted it sharply. As he crashed to the ground, Bond rolled agilely and, with a motion that he hoped went unnoticed, moved his fountain pen deeper into his breast pocket.
+  “Search him.” barked Le Chiffre.
+  The two gunmen dragged Bond to his feet. While the thin man kept his gun trained on Bond’s unquiet chest, the Corsican roughly stripped Bond’s revolver out of its shoulder holster. He twisted Bond around brusquely in search of other weaponry. Le Chiffre observed his assistant’s work attentively. Then, as if reading Bond’s thoughts, he crossed the room and said, “Come my dear friend. Let’s not waste time.”"""
+
+experiment_B_unused_comb = """Filled with confidence after defeating Le Chiffre, Bond had extended an invitation to Vesper to dine with him in the hotel restaurant. She had cheerfully accepted.
+
+Bond looked in the mirror of his hotel room to make certain that his black tie was centered in his collar. He noticed that he had a white thread on his lapel, and removed it. Bond smiled at the elegant figure he presented.
+
+As Bond turned to leave the room, the door burst in toward him. Three large men leapt through, guns drawn. The largest of the men was Le Chiffre, Bond's recently vanquished opponent.
+
+As the three men approached him, Bond suddenly whirled around and caught one of the gunmen squarely in the stomach with a well-placed shove of his heel. Unfortunately, the second gunman had accurately judged the rest of Bond’s motion. He caught Bond’s shoe at the top of its arc and twisted it sharply. As he crashed to the ground, Bond rolled agilely and, with a motion in which he took great pride, he righted himself with minimal damage. Bond was unharmed, but he was trapped.
+
+“Search him.” barked Le Chiffre.
+
+The two gunmen carefully searched Bond. They roughly stripped Bond’s revolver out of its shoulder holster. They twisted him around in search of other weaponry.
+
+Le Chiffre observed his assistants' work attentively. He crossed the room to Bond, and patted him down once again. Le Chiffre pulled out Bond’s pocket comb and ran his finger down its teeth. He smiled broadly, and flipped the comb well out of Bond’s reach. “Come, my dear friend,” said Le Chiffre. “Let’s not waste time.”
+
+Bond felt puny and impotent."""
+
+experiment_B_used_comb = """Filled with confidence after defeating Le Chiffre, Bond had extended an invitation to Vesper to dine with him in the hotel restaurant. She had cheerfully accepted.
+
+Bond looked in the mirror of his hotel room to make certain that his black tie was centered in his collar. He noticed that his hair was just the least bit mussed, so he extracted his comb from his pocket and smoothed his wandering locks back into place.
+
+As Bond turned to leave the room, the door burst in toward him. Three large men leapt through, guns drawn. The largest of the men was Le Chiffre, Bond's recently vanquished opponent.
+
+As the three men approached him, Bond suddenly whirled around and caught one of the gunmen squarely in the stomach with a well-placed shove of his heel. Unfortunately, the second gunman had accurately judged the rest of Bond’s motion. He caught Bond’s shoe at the top of its arc and twisted it sharply. As he crashed to the ground, Bond rolled agilely and, with a motion in which he took great pride, he righted himself with minimal damage. Bond was unharmed, but he was trapped.
+
+“Search him.” barked Le Chiffre.
+
+The two gunmen carefully searched Bond. They roughly stripped Bond’s revolver out of its shoulder holster. They twisted him around in search of other weaponry.
+
+Le Chiffre observed his assistants' work attentively. He crossed the room to Bond, and patted him down once again. Le Chiffre pulled out Bond’s pocket comb and ran his finger down its teeth. He smiled broadly, and flipped the comb well out of Bond’s reach. “Come, my dear friend,” said Le Chiffre. “Let’s not waste time.”
+
+Bond felt puny and impotent."""
+
+experiment_C_prior_soution_not_mentioned = """Blofeld smiled and said, “We must stop meeting like this Mr. Bond. I grow weary of pointing a gun at you.”
+  Before Bond could reply, a large bird crashed into the wall of glass that made up one side of Blofeld’s large office. Bond took advantage of the distraction to pull up one corner of the rug on which Blofeld stood. Blofeld stumbled backwards and his gun shot up into the air, knocking out the large light that had illuminated the room.
+  As Bond’s eyes grew accustomed to the dark of the office, he hurried toward the door. He was met there, however, by a very large and very ugly man. Light from the hall spilled in, revealing another gun pointing at his chest.
+  Blofeld picked himself up off the ground and said, “I am not amused by your antics Mr. Bond. I hope you understand that my good friend here, Mr. Crushak, is devoted to eliminating such irritations from my life.” At this, the large man contorted his face into what he might have intended as a smile. It made him look no less ugly. Blofeld continued, “Tie up Mr. Bond.”
+  Crushak forced Bond into a wooden arm chair and carefully pinned Bond in place by wrapping a piece of piano wire around each of the chair’s corners and Bond’s arms. Each subsequent twist of the wire bit more painfully into Bond’s flesh.
+  Crushak grunted to indicate that he was done. Blofeld said, “My dear Mr. Bond. You came here as my guest and now I find you going through my personal belongings. I don’t think you have behaved very well. I will leave you here with Mr. Crushak to contemplate your rude behavior.”"""
+
+experiment_C_prior_solution_mentioned_and_removed = """Blofeld smiled and said, “We must stop meeting like this Mr. Bond. I grow weary of pointing a gun at you.”
+Before Bond could reply, a large bird crashed into the wall of glass that made up one side of Blofeld’s large office. Bond took advantage of the distraction to pull up one corner of the rug on which Blofeld stood. Blofeld stumbled backwards and his gun shot up into the air, knocking out the large light that had illuminated the room.
+As Bond’s eyes grew accustomed to the dark of the office, he hurried toward the door. He was met there, however, by a very large and very ugly man. Light from the hall spilled in, revealing another gun pointing at his chest.
+Blofeld picked himself up off the ground and said, “I am not amused by your antics Mr. Bond. I hope you understand that my good friend here, Mr. Crushak, is devoted to eliminating such irritations from my life.” At this, the large man contorted his face into what he might have intended as a smile. It made him look no less ugly. Blofeld continued, “Tie up Mr. Bond.”
+Crushak forced Bond into a wooden arm chair and carefully pinned Bond in place by wrapping a piece of piano wire around each of the chair’s corners and Bond’s arms. Each subsequent twist of the wire bit more painfully into Bond’s flesh.
+Crushak grunted to indicate that he was done. Blofeld said, “My dear Mr. Bond. The last time I held you in captivity, you were able to outwit my guard. He died soon after that in an automobile accident. Poor fellow. Crushak here will be responsible for you this time. He has orders to shoot you if you even attempt to speak to him.”
+Blofeld left the room with his shoulders set in an attitude of triumph."""
+
+experiment_C_prior_solution_mentioned_not_removed = """Blofeld smiled and said, “We must stop meeting like this Mr. Bond. I grow weary of pointing a gun at you.”
+Before Bond could reply, a large bird crashed into the wall of glass that made up one side of Blofeld’s large office. Bond took advantage of the distraction to pull up one corner of the rug on which Blofeld stood. Blofeld stumbled backwards and his gun shot up into the air, knocking out the large light that had illuminated the room.
+As Bond’s eyes grew accustomed to the dark of the office, he hurried toward the door. He was met there, however, by a very large and very ugly man. Light from the hall spilled in, revealing another gun pointing at his chest.
+Blofeld picked himself up off the ground and said, “I am not amused by your antics Mr. Bond. I hope you understand that my good friend here, Mr. Crushak, is devoted to eliminating such irritations from my life.” At this, the large man contorted his face into what he might have intended as a smile. It made him look no less ugly. Blofeld continued, “Tie up Mr. Bond.”
+Crushak forced Bond into a wooden arm chair and carefully pinned Bond in place by wrapping a piece of piano wire around each of the chair’s corners and Bond’s arms. Each subsequent twist of the wire bit more painfully into Bond’s flesh.
+  Crushak grunted to indicate that he was done. Blofeld said, “My dear Mr Bond. The last time I held you in captivity, you were able to outwit my guard. He died soon after that in an automobile accident. Poor fellow. Crushak here will be responsible for you this time.”
+Blofeld left the room with his shoulders set in an attitude of triumph."""
+
+
+prompts = {
+    "Experiment A": experiment_A_prompt,
+    "Experiment B": experiment_B_prompt,
+    "Experiment C": experiment_C_prompt
+}
+
+version_prompts = {
+    "Experiment A": [experiment_A_pen_not_mentioned,
+                     experiment_A_pen_mentioned_removed,
+                     experiment_A_pen_mentioned_not_removed],
+    "Experiment B": [experiment_B_unused_comb],
+    "Experiment C": [experiment_C_prior_soution_not_mentioned,
+                     experiment_C_prior_solution_mentioned_and_removed,
+                     experiment_C_prior_solution_mentioned_not_removed]
+}
+
+# Experiments A, B, C Alternative Parameters:
+# Replications of Experiments 1, 2 and 3 as A, B, and C by using  different character names and settings to remove prior knowledge associated.
+# Experiments A & B: Hero renamed to "Mers," villain to "Chiffrex," book titled "Meeting at Midnight."
+# Experiment C: Changes to characters renamed to "Kalweitz" and book titled "Twenty Miles to the Sea."
