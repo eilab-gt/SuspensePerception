0b00dbafae54c392613ba880e80a5ed884e27f29
Devin Zhang <devinzhang415@gmail.com>
2024-08-05 21:41:32 -0400

    Code cleanup
    * Added code documentation, comments, and type hints
    * Add top_p, top_k, repetition_penalty, stop, and stream parameters to config / arg input
    * Cleaned up and removed extraneous files

--- src/thriller/utils.py
+++ src/thriller/utils.py
@@ -1,16 +1,34 @@
+"""
+File IO, loading configuration files and saving output files
+"""
+
 import json
 from pathlib import Path
-
 import pandas as pd
 import yaml
+import io
 
 
-def load_config(config_path):
+def load_config(config_path: str) -> io.TextIOWrapper:
+    """
+    Open and return the configuration file
+    Args:
+        config_path: path to the configuration yaml file
+    Return:
+        Opened configuration file
+    """
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
 
 
-def save_raw_api_output(output, filename, output_path: Path):
+def save_raw_api_output(output: str, filename: str, output_path: Path) -> None:
+    """
+    Save text to a JSON file
+    Args:
+        output: data to save 
+        filename: name of the output JSON file
+        output_path: path to the output directory
+    """
     raw_output_dir = Path(output_path) / "raw_outputs"
     raw_output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -18,7 +36,15 @@ def save_raw_api_output(output, filename, output_path: Path):
         json.dump(output, f, indent=2)
 
 
-def process_and_save_results(results, output_path: Path):
+def process_and_save_results(results: list[dict[str, str]], output_path: Path) -> pd.DataFrame:
+    """
+    Save data to a dataframe and save as parquet
+    Args:
+        results: data to save
+        output_path: path to the output directory
+    Return:
+        The data as a dataframe
+    """
     output_path = Path(output_path)
     output_path.mkdir(parents=True, exist_ok=True)
 
